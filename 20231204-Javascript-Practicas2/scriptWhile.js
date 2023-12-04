9d071d3f5f2d4c4138434fe5f224505581091676
maugallo <maugallok@gmail.com>
2023-12-04 11:27:57 -0300

    Ejercicios con while

--- 20231204-Javascript-Practicas2/scriptWhile.js
+++ 20231204-Javascript-Practicas2/scriptWhile.js
@@ -1,3 +1,5 @@
+//Ejercicio 1:
+/*
 let num = Number(prompt("Ingrese un número"));
 let cant = 0;
 let result = 0;
@@ -5,12 +7,30 @@ let result = 0;
 if (!isNaN(num)){
     while (cant <= num) {
         result += cant;
-        cant += 1;
+        cant ++;
     }
-    alert(`El promedio de la suma de los números anteriores a ${num} es ${Number(result/cant).toFixed(1)}`);
+    alert(`El promedio de ${num} es ${Number(result/cant).toFixed(1)}`);
 } else{
     alert("El valor ingresado no es un número!");
 }
+*/
 
+//Ejercicio 2:
+let continuar = true;
+let cant = 0;
+let num;
 
+while (continuar){
+    num = Number(prompt("Ingrese un número entero"));
+    if (!isNaN(num)){
+        if (num < 0){
+            continuar = false;
+        } else{
+            cant ++;
+        }
+    } else{
+        alert("Ingresa un número válido");
+    }
+}
 
+alert(`Programa terminado! Ingresaste ${cant} números enteros.`);
