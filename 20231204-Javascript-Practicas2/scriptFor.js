d7fc43ed202df6da4f8cceaf409290aea93d3db7
maugallo <maugallok@gmail.com>
2023-12-04 16:42:36 -0300

    Ejercicios con for

---
+++ 20231204-Javascript-Practicas2/scriptFor.js
@@ -0,0 +1,28 @@
+//Ejercicio 1:
+/*
+let num = Number(prompt("Ingrese el número"));
+let simbolo = "*";
+let indexMax = num -1;
+
+for (let index = 0; index < num; index++) {
+    console.log(simbolo);
+    simbolo += "*";
+}
+
+for(index = num; index > 0; index--){
+    console.log(simbolo.substring(0, indexMax));
+    indexMax --;
+}
+*/
+
+//Ejercicio 2:
+/* 
+let num = Number(prompt("Ingrese el número"));
+let factorial = num;
+
+for(let index = num-1; index > 0; index--){
+    factorial = factorial * index;
+}
+
+alert(`El factorial de ${num} es ${factorial}`);
+*/
