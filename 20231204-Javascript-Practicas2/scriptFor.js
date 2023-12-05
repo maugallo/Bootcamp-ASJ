dd660490abcdc4da65abeed972363902af154bf7
maugallo <maugallok@gmail.com>
2023-12-05 09:15:11 -0300

    Ejercicios con for

--- 20231204-Javascript-Practicas2/scriptFor.js
+++ 20231204-Javascript-Practicas2/scriptFor.js
@@ -1,5 +1,5 @@
 //Ejercicio 1:
-
+/* 
 let num = Number(prompt("Ingrese el número"));
 let simbolo = "";
 
@@ -8,14 +8,14 @@ for (let index = 0; index <= num; index++) {
     simbolo += "*";
 }
 
-for(let index = num; index > 0; index--){
+for(let index = num; index > 0; index--){ //El primer for, ayuda a determinar la cantidad de "*" que el segundo for debe dibujar. Y al final del segundo for, dibujará la cantidad de símbolos necesarios.
     simbolo = "";
-    for(let index2 = 0; index2 < index-1; index2++){
+    for(let index2 = 0; index2 < index-1; index2++){ //El segundo for se basará en la cantidad de valores que el index del primer for tenga, y concatenará los "*".
         simbolo += "*";
     }
     console.log(simbolo);
 }
-
+ */
 
 //Ejercicio 2:
 /* 
